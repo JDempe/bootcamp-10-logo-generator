b10bcd011d2ea9bacb73a428745c1c74d6bbd3de
JDempe <123279032+JDempe@users.noreply.github.com>
2023-04-17 15:28:30 -0400

    Added SVG generation and tests

--- index.js
+++ index.js
@@ -1,61 +1,80 @@
-// Inquirer prompts to get user input on shape (circle, square, triangle), shape color, text (1-3 characters), and text color
-// outputs to logo.svg
+// Description: This is the main file for the logo generator.  It will prompt the user for input and then generate the logo.
 
+// Import third party modules
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// The following code is from https://www.npmjs.com/package/css-color-names
-const csscolors = require('css-color-names');
-const cssColorKeywords = Object.keys(csscolors);
+
+// Import custom classes
+// Answer validation class
+const { answerValidation } = require("./lib/answerValidation");
+const answerValidation = new answerValidation();
+
+// Shape classes
+const { Square, Triangle, Circle } = require("./lib/shapes");
+const { Text } = require("./lib/text");
 
 const questions = [
-   {
-      type: 'list',
-      name: 'shape',
-      message: 'What shape would you like?',
-      choices: ['circle', 'square', 'triangle'],
-   },
-   {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'What color would you like the shape?  Enter a color keyword or hex code.',
-      validate: colorValidation,
-   },
-   {
-      type: 'input',
-      name: 'text',
-      message: 'What text would you like?',
-      validate: function (value) {
-         var pass = value.match(/^[0-9A-Z]{1,3}$/i);
-         if (pass) {
-            return true;
-         }
-
-         return 'Please enter 1-3 characters.';
-      },
-   },
-   {
-      type: 'input',
-      name: 'textColor',
-      message: 'What color would you like the text?  Enter a color keyword or 6 character hex code, please include the hash mark! (e.g. #FFFFFF)', 
-      validate: colorValidation,
-   },
+  {
+    type: "list",
+    name: "shape",
+    message: "What shape would you like?",
+    choices: ["circle", "square", "triangle"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message:
+      "What color would you like the shape?  Enter a color keyword or hex code.",
+    validate: answerValidation.colorValidation,
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "What text would you like?",
+    validate: answerValidation.textValidation,
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message:
+      "What color would you like the text?  Enter a color keyword or 6 character hex code, please include the hash mark! (e.g. #FFFFFF)",
+    validate: answerValidation.colorValidation,
+  },
 ];
 
-// TODO import?  import all validations?  so we can test them?
-function colorValidation(value) {
-   if (value.match(/^#[0-9A-F]{6}$/i) || cssColorKeywords.includes(value)){
-      return true;
-   }
-   return 'Please enter a valid keyword or hex code.  Go to https://www.w3schools.com/colors/colors_names.asp for a list of keywords or https://www.w3schools.com/colors/colors_picker.asp for a hex code.';
-}
+inquirer.prompt(questions).then((answers) => {
+  // Display Results
+  console.log(answers);
 
-inquirer.prompt(questions).then(answers => {
-   console.log(answers);
-   // fs.writeFile("logo.svg", answers, function(err) {
-   //    if (err) {
-   //       return console.log(err);
-   //    }
-   //    console.log("The file was saved!");
-   // });
+  // Determine which shape was selected and create the shape class
+  switch (answers.shape) {
+    case "circle":
+      var shape = new Circle(answers.shapeColor);
+      break;
+    case "square":
+      var shape = new Square(answers.shapeColor);
+      break;
+    case "triangle":
+      var shape = new Triangle(answers.shapeColor);
+      break;
+    default:
+      console.log("Something went wrong.");
+      break;
+  }
+
+  //   Create text class for rendering text
+  const text = new Text(answers.text, answers.textColor);
+
+  const finalSVG = `<svg width="300" height="200" style="background-color:white">
+   ${shape.render()}
+   ${text.render()}
+   </svg>`;
+
+  fs.writeFile("logo.svg", finalSVG, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
 });
